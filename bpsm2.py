071f0fc838998a0ecc054d86aaa9671444cdce88
B193818 <hello>
2021-11-20 11:07:11 +0000

    do blast analysis and read blast result

--- bpsm2.py
+++ bpsm2.py
@@ -113,4 +113,66 @@ while True:
         num = stop
     break
 
+## Scale down sequences
+if num >= stop:  # user input a larger number
+    analysis_seq = align
+    print('Analysis all of {} sequences'.format(stop))
+else:  # user wants to scale down sequences
+    print('Analysis most similar {} sequences'.format(num))
+    # info = '{}/protein-sequences-info.txt'.format(workspace)
+    # args = ['infoalign', '-sequence', align, '-outfile', info]
+    # subprocess.check_call(args)
+    # build path for consensus sequence
+    cons = '{}/protein-sequences-cons.fa'.format(workspace)
+    # build cons arguments
+    args = ['cons', '-sequence', align, '-outseq', cons]
+    # call cons
+    subprocess.check_call(args)
+    print('Consensus sequences saved in: "{}"'.format(cons))
 
+    # make blast db and blast sequence
+    print('Making blast db ...')
+    # build path for blast db
+    db = '{}/protein-sequences-db'.format(workspace)
+    # build makeblastdb arguments
+    args = ['makeblastdb', '-in', fasta, '-dbtype', 'prot', '-out', db]
+    # call makeblastdb
+    subprocess.check_call(args)
+    print('Blast all sequences ...')
+    # build path for blastp result
+    blastp = '{}/protein-sequences-blastp.txt'.format(workspace)
+    # build blastp arguments
+    args = [
+        'blastp', '-db', db, '-query', cons, '-outfmt', '7', '-max_hsps', '1'
+    ]
+    # save blastp result to file
+    with open(blastp, 'w') as stdout:
+        # call blastp
+        subprocess.check_call(args, stdout=stdout)
+    print('Blastp result saved in: "{}"'.format(blastp))
+
+    # collect bit score and sequence name
+    seq_names = []
+    # read blastp result
+    with open(blastp, 'r') as f:
+        for seq_line in f:  # loop through blastp result
+            if not seq_line.startswith('#'):  # skip header
+                seq_line = seq_line.strip().split('\t')  # split fields
+                # collect bit score and sequence name
+                seq_names.append((float(seq_line[11]), seq_line[1]))
+    # sorted by bit score and extract most highest bit score sequences
+    seq_names = sorted(seq_names, reverse=True)[:num]
+    # output most similarly sequence name
+    order = '{}/protein-sequences-order.txt'.format(workspace)
+    with open(order, 'w') as f:
+        for _, name in seq_names:  # only output sequence name
+            f.write('{}\n'.format(name))
+    # build path for pullseq output
+    analysis_seq = '{}/protein-sequences-analysis.fa'.format(workspace)
+    # build pullseq arguments
+    args = [PULLSEQ, '-i', align, '-n', order]
+    # write pullseq output to a file
+    with open(analysis_seq, 'w') as stdout:
+        # call pullseq
+        subprocess.check_call(args, stdout=stdout)
+    print('Analysis sequence saved in: "{}"'.format(analysis_seq))
